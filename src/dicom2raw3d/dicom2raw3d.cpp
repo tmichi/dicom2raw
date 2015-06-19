9088ec41991264deaea10e13cb099900d6fbbbc6
Takashi Michikawa <michi@MichikawaTakashi-no-MacBook.local>
2015-06-19 19:13:18 +0900

    sorted

--- src/dicom2raw3d/dicom2raw3d.cpp
+++ src/dicom2raw3d/dicom2raw3d.cpp
@@ -6,83 +6,93 @@
 #include <string>
 #include <dtr.hpp>
 
-int create_output_file_name( const std::vector<std::string> & input_files, std::string& output_file )
-{
-    if( input_files.size() == 0 ) {
-        std::cerr<<"[error] number of DICOM files is zero"<<std::endl;
-        return -1;
-    }
-    std::ifstream fin( input_files[0].c_str(), std::ios::in | std::ios::binary );
-    if ( ! fin ) {
-        std::cerr << "[error]: Could not open file [" << input_files[0] << "]\n";
-        return -1;
-    }
-    dtr::File file;
-    std::string error = file.parse( fin );
-    if ( ! error.empty() ) {
-        std::cerr << "[error]: " << error << "\n";
-        return -1;
-    }
-    const dtr::Information& information = file.get_information();
-    int sx = information.get_rows();
-    int sy = information.get_columns();
-    int sz = input_files.size();
-    double px = information.get_pixel_spacing_row();
-    double py = information.get_pixel_spacing_column();
-    double pz = information.get_spacing_between_slices();
-
-    double ox = information.get_image_position_patient_x();
-    double oy = information.get_image_position_patient_y();
-    double oz = information.get_image_position_patient_z();
-    std::cerr<<"[info] origin position : ("<<ox<<", "<<oy<<", "<<oz<<")"<<std::endl;
-    std::stringstream ss;
-
-    //
-    std::string output_header;
-    output_header = input_files[0];
-    ss<<output_header<<"-"<<sx<<"x"<<sy<<"x"<<sz<<"-"<<px<<"x"<<py<<"x"<<pz<<".raw";
-    output_file = ss.str();
-    std::cerr<< "[info] bits stored: " << information.get_bits_stored() << std::endl;
-    return 1;
-}
-
 int main( int argc, char* argv[] )
 {
-    if( argc == 1 ) {
-        std::cerr << argv[0] << " input0.dcm input1.dcm ...."<<std::endl;
-        std::cerr << argv[0] << " *.dcm "<<std::endl;
-        return 1;
-    }
-    std::vector<std::string> input_files;
-    for( int i = 1 ; i < argc ; ++i ) {
-        input_files.push_back( std::string( argv[i] ) );
-    }
-
-    std::string output_file;
-    if( ! create_output_file_name( input_files, output_file ) ) {
-        return -1;
-    }
-
-    std::cerr<<"saving raw file to "<<output_file<<std::endl;
-    std::ofstream fout( output_file.c_str(), std::ios::binary );
-    if( !fout ) {
-        std::cerr << "[error]: could not open file [" << output_file << "]\n";
-        return -1;
-    }
-    for( int i = 0 ; i < static_cast<int>( input_files.size() ) ; ++i ) {
-        std::ifstream fin( input_files[i].c_str(), std::ios::in | std::ios::binary );
-        if ( !fin ) {
-            std::cerr << "[error]: could not open file [" << input_files[i] << "]\n";
-            return -1;
-        }
-        dtr::File file;
-        std::string error = file.parse( fin );
-        if ( ! error.empty() ) {
-            std::cerr << "[error]: " << error << "\n";
-            return -1;
-        }
-        const dtr::Information& info = file.get_information();
-        fout.write( static_cast<const char*>( file.get_pixel_data() ), file.get_pixel_data_length() );
-    }
-    return 0;
+	if( argc == 1 ) {
+		std::cerr << argv[0] << " input0.dcm input1.dcm ...."<<std::endl;
+		std::cerr << argv[0] << " *.dcm "<<std::endl;
+		return 1;
+	}
+	std::vector<std::string> files;
+	for( int i = 1 ; i < argc ; ++i ) {
+		files.push_back( std::string( argv[i] ) );
+	}
+	
+	std::string output_file;
+	
+	if( files.size() == 0 ) {
+		std::cerr<<"[error] number of DICOM files is zero"<<std::endl;
+		return -1;
+	}
+	
+	int sx, sy, sz;
+	double px, py, pz;
+	double ox, oy, oz;
+	int voxelSize = 1;
+	
+	std::vector<std::pair<double, int> > sorted_indices;
+	for ( int i = 0 ; i < files.size() ; ++i ) {
+		const std::string& dicomFile = files.at( i );
+		std::ifstream fin( dicomFile.c_str(), std::ios::in | std::ios::binary );
+		if ( ! fin ) {
+			std::cerr << "[error]: Could not open file [" << files.at( i ) << "]\n";
+			return -2;
+		}
+		dtr::File file;
+		std::string error = file.parse( fin );
+		if ( ! error.empty() ) {
+			std::cerr << "[error]: " << error << "\n";
+			return -2;
+		}
+		const dtr::Information& information = file.get_information();
+		if ( i == 0 ) {
+			voxelSize = static_cast<int> ( ( information.get_bits_stored() -1 ) / 8 ) + 1  ;
+			std::cout<<"voxel size: "<<voxelSize<<std::endl;
+			
+			sx = information.get_rows();
+			sy = information.get_columns();
+			sz = static_cast<int>(files.size());
+			
+			px = information.get_pixel_spacing_row();
+			py = information.get_pixel_spacing_column();
+			pz = information.get_spacing_between_slices();
+			
+			ox = information.get_image_position_patient_x();
+			oy = information.get_image_position_patient_y();
+		}
+		sorted_indices.push_back ( std::make_pair( oz, i ) );
+	}	    
+	// sort indices by z coordinate
+	std::sort ( sorted_indices.begin(), sorted_indices.end() );
+	oz = sorted_indices[0].first;
+	std::cout<<"origin:"<<ox<<","<<oy<<","<<oz<<std::endl;
+	std::string output_header = files[0];
+	std::stringstream ss;
+	ss<<output_header<<"-"<<sx<<"x"<<sy<<"x"<<sz<<"-"<<px<<"x"<<py<<"x"<<pz<<".raw";
+	output_file = ss.str();   
+	std::cerr<<"saving raw file to "<<output_file<<std::endl;
+	
+	std::ofstream fout( output_file.c_str(), std::ios::binary );
+	if( !fout ) {
+		std::cerr << "[error]: could not open file [" << output_file << "]\n";
+		return -1;
+	}
+	for( int i = 0 ; i < static_cast<int>( files.size() ) ; ++i ) {
+		std::string& slice_name =  files[sorted_indices[i].second];
+		std::cerr<<slice_name<<" "<<sorted_indices[i].first<<std::endl;
+		std::ifstream fin(slice_name.c_str(), std::ios::in | std::ios::binary );
+		if ( !fin ) {
+			std::cerr << "[error]: could not open file [" << slice_name << "]\n";
+			return -1;
+		}
+		dtr::File file;
+		std::string error = file.parse( fin );
+		if ( ! error.empty() ) {
+			std::cerr << "[error]: " << error << "\n";
+			return -1;
+		}
+		const dtr::Information& info = file.get_information();
+		fout.write( static_cast<const char*>( file.get_pixel_data() ), file.get_pixel_data_length() );
+	}
+	return 0;
 }
